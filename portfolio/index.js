082443d9ba463e5e1699fbb1855d1fefe7514015
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-01-20 20:17:13 -0500

    feat: add side drawer

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,4 @@
 document.querySelector(".burger").addEventListener("click", () => {
   document.querySelector(".burger").classList.toggle("open");
+  document.querySelector(".nav").classList.toggle("open");
 });
