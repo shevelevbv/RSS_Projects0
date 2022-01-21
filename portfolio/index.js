da38fb9eb201a70bb3089058566d95d8e752a5ad
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-01-20 21:08:33 -0500

    feat: add menu animations

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,4 +1,15 @@
+function closeMenu() {
+  if (event.target.classList.contains('nav-item')) {
+    document.querySelector(".burger").classList.remove("open");
+    document.querySelector(".nav").classList.remove("open");
+  }
+}
+
 document.querySelector(".burger").addEventListener("click", () => {
   document.querySelector(".burger").classList.toggle("open");
   document.querySelector(".nav").classList.toggle("open");
+});
+
+document.querySelector(".nav").addEventListener("click", () => {
+  closeMenu();
 });
