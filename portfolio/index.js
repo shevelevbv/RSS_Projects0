779154abdf682fa75934604505d47ec023b4955a
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-01-26 13:32:05 -0500

    feat: add picture update

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,8 +1,14 @@
-function closeMenu() {
+function closeMenu(event) {
   if (event.target.classList.contains('nav-item')) {
     document.querySelector(".burger").classList.remove("open");
     document.querySelector(".nav").classList.remove("open");
     document.querySelector(".page-mask").classList.remove("open");
+  }
+}
+
+function changeImage(event) {
+  if(event.target.classList.contains("button")) {
+    images.forEach((img, index) => img.src = `./img/jpg/${event.target.dataset.season}/${index + 1}.jpg`);
   }
 }
 
@@ -12,9 +18,11 @@ document.querySelector(".burger").addEventListener("click", () => {
   document.querySelector(".page-mask").classList.toggle("open");
 });
 
-document.querySelector(".nav").addEventListener("click", () => {
-  closeMenu();
-});
+document.querySelector(".nav").addEventListener("click", closeMenu);
+
+const images = document.querySelectorAll(".portfolio-image");
+
+document.querySelector(".button-container").addEventListener("click", changeImage);
 
 console.log("Самооценка: 75 баллов\n" + 
             "Верстка соответствует макету при ширине 768px: 48 баллов\n" +
