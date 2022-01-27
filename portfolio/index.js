921d8777de341485e1a63805ce8004b49361920e
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-01-27 15:00:23 -0500

    feat: add translation

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,22 +1,43 @@
+import i18Obj from './translate.js';
+
 function changeClassActive (event) {
-  if(event.target.classList.contains("portfolio__button")) {
-    imageBtns.forEach(btn => btn.classList.remove("active"));
-    event.target.classList.add("active");
+  if(event.target.classList.contains('portfolio__button')) {
+    imageBtns.forEach(btn => btn.classList.remove('active'));
+    event.target.classList.add('active');
+  }
+  if(event.target.classList.contains('lang')) {
+    langSwitches.forEach(lang => lang.classList.remove('active'));
+    event.target.classList.add('active');
   }
 }
 
 function changeImage(event) {
-  if(event.target.classList.contains("portfolio__button")) {
+  if(event.target.classList.contains('portfolio__button')) {
     images.forEach((img, index) => img.src = `./img/jpg/${event.target.dataset.season}/${index + 1}.jpg`);
+  }
+}
+
+function changeLanguage(event) {
+  if(event.target.classList.contains('lang')) {
+    language = event.target.classList.contains('language__ru') ? 'ru' : 'en'; 
   }
 }
 
 function closeMenu(event) {
   if (event.target.classList.contains('nav__link')) {
-    document.querySelector(".burger").classList.remove("open");
-    document.querySelector(".nav").classList.remove("open");
-    document.querySelector(".page-mask").classList.remove("open");
+    document.querySelector('.burger').classList.remove('open');
+    document.querySelector('.nav').classList.remove('open');
+    document.querySelector('.page-mask').classList.remove('open');
   }
+}
+
+function getTranslate(language) {
+  const objWithDataAttr = document.querySelectorAll('[data-i18n]');
+  objWithDataAttr.forEach(obj => {
+    if (i18Obj[language].hasOwnProperty(obj.dataset.i18n)) {
+      obj.textContent = i18Obj[language][obj.dataset.i18n];
+    }
+  });
 }
 
 function preloadImages(seasons) {
@@ -28,25 +49,34 @@ function preloadImages(seasons) {
   });
 }
 
-document.querySelector(".burger").addEventListener("click", () => {
-  document.querySelector(".burger").classList.toggle("open");
-  document.querySelector(".nav").classList.toggle("open");
-  document.querySelector(".page-mask").classList.toggle("open");
+const langSwitches = document.querySelectorAll('.lang');
+let language = 'en';
+
+document.querySelector('.language').addEventListener('click', changeClassActive);
+document.querySelector('.language').addEventListener('click', changeLanguage);
+document.querySelector('.language').addEventListener('click', () => { 
+  getTranslate(language);
 });
 
-document.querySelector(".nav").addEventListener("click", closeMenu);
+document.querySelector('.burger').addEventListener('click', () => {
+  document.querySelector('.burger').classList.toggle('open');
+  document.querySelector('.nav').classList.toggle('open');
+  document.querySelector('.page-mask').classList.toggle('open');
+});
 
-const seasons = ["winter", "spring", "summer", "autumn"];
+document.querySelector('.nav').addEventListener('click', closeMenu);
+
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
 preloadImages(seasons);
 
-const images = document.querySelectorAll(".portfolio__image");
-const imageBtns = document.querySelectorAll(".portfolio__button");
+const images = document.querySelectorAll('.portfolio__image');
+const imageBtns = document.querySelectorAll('.portfolio__button');
 
-document.querySelector(".portfolio__buttons").addEventListener("click", changeClassActive);
+document.querySelector('.portfolio__buttons').addEventListener('click', changeClassActive);
 
-document.querySelector(".portfolio__buttons").addEventListener("click", changeImage);
+document.querySelector('.portfolio__buttons').addEventListener('click', changeImage);
 
-console.log("Самооценка: 75 баллов\n" + 
-            "Верстка соответствует макету при ширине 768px: 48 баллов\n" +
-            "Горизонтальная полоса прокрутки отсутствует на расширениях 320-1440px: 15 баллов\n" +
-            "Адаптивное меню при ширине экрана <= 768px : 22 балла\n");
+console.log('Самооценка: 75 баллов\n' + 
+            'Верстка соответствует макету при ширине 768px: 48 баллов\n' +
+            'Горизонтальная полоса прокрутки отсутствует на расширениях 320-1440px: 15 баллов\n' +
+            'Адаптивное меню при ширине экрана <= 768px : 22 балла\n');
