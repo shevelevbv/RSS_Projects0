2fb28f0063549f46be6abe9f695e834b757cd0c6
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-02-11 13:43:29 -0500

    feat: add autofocus and disable autocomplete

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,10 +1,20 @@
 function changeClassActive(event) {
   if (event.target.classList.contains('search__input')) {
     inputContainer.classList.add('active');
+    if (inputField.value.trim() === "") {
+      inputContainer.classList.remove('active');
+    }
   }
 }
 
 const inputContainer = document.querySelector('.search');
 const inputField = document.querySelector('.search__input');
+const abortButton = document.querySelector('.search__abort');
 
 inputField.addEventListener('input', changeClassActive);
+
+abortButton.addEventListener('click', () => {
+  inputField.value="";
+  inputContainer.classList.remove('active');
+  inputField.focus();
+});
