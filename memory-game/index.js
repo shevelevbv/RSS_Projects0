0c59e26109d88f802d657b780668d5ce1195aab8
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-02-19 12:10:38 -0500

    feat: add score

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,7 +1,12 @@
 const cards = document.querySelectorAll('.card');
+const scoreScreen = document.querySelector('.score-number');
 let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
+let matchCounter = 0;
+let tryCounter = 0;
+let userScore = 3;
+let points = 0;
 
 function flipCard(e) {
   if (lockBoard) return;
@@ -20,8 +25,19 @@ function flipCard(e) {
 }
 
 function checkForMatch() {
-  let isMatch = firstCard.dataset.language === secondCard.dataset.language;
-   isMatch ? disableCards() : unflipCards();                                                           
+  tryCounter++;
+  if (firstCard.dataset.language === secondCard.dataset.language) {
+    disableCards();
+    matchCounter++;
+    updateScore('+');
+    if (matchCounter === 10) {
+      endGame();
+    }
+    return;
+  }
+  
+  updateScore('-');
+  unflipCards();                                                           
 }
 
 function disableCards() {
@@ -29,6 +45,10 @@ function disableCards() {
   secondCard.removeEventListener('click', flipCard);
 
   resetBoard();
+}
+
+function endGame() {
+  console.log('won');
 }
 
 function resetBoard() {
@@ -45,10 +65,41 @@ function unflipCards() {
   }, 1000);
 }
 
+function updateScore(sign) {
+  if (sign === '+') {
+    if (points < 0) {
+      points = 0;
+    }
+      points++;
+    } else if (sign === '-') {
+      if (points > 0) {
+        points = 0;
+      }
+      points--;
+    } 
+  userScore += points;
+}
+
 (function shuffle() {
   cards.forEach(card => {
     let randomNum = Math.floor(Math.random() * 20);
     card.style.order = randomNum;
+  });
+})();
+
+(function showAll() {
+  lockBoard = true;
+  cards.forEach(card => card.removeEventListener('click', flipCard));
+  cards.forEach(card => {
+    setTimeout(() => {
+      card.classList.add('flip');
+    }, 1000);
+  });
+  cards.forEach(card => {
+    setTimeout(() => {
+      card.classList.remove('flip');
+      resetBoard();
+    }, 4000);
   });
 })();
 
