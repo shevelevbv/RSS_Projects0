ba3af00f81ae2c21c213e7f64a51bd9877b98de4
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-02-18 23:14:56 -0500

    feat: complete basic logic

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,16 +1,55 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
 function flipCard(e) {
+  if (lockBoard) return;
+  if (this === firstCard) return;
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
+    return;
   }
+
+  secondCard = this;
+
+  checkForMatch();
 }
 
-const cards = document.querySelectorAll('.card');
-let hasFlippedCard = false;
-let firstCard, secondCard;
+function checkForMatch() {
+  let isMatch = firstCard.dataset.language === secondCard.dataset.language;
+   isMatch ? disableCards() : unflipCards();                                                           
+}
 
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
 
+  resetBoard();
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    resetBoard();
+  }, 1000);
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomNum = Math.floor(Math.random() * 20);
+    card.style.order = randomNum;
+  });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
